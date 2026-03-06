dc6c7780d03963805dcb6aeb1e59bc767c1ef1d1
Mohd Arif Ansari <38493289+Arif0000@users.noreply.github.com>
2026-03-07 01:27:50 +0530

    Update three-network.js

--- js/three-network.js
+++ js/three-network.js
@@ -1,14 +1,13 @@
+const scene = new THREE.Scene();
 
-const scene=new THREE.Scene();
-
-const camera=new THREE.PerspectiveCamera(
+const camera = new THREE.PerspectiveCamera(
 75,
 window.innerWidth/window.innerHeight,
 0.1,
 1000
 );
 
-const renderer=new THREE.WebGLRenderer({alpha:true});
+const renderer = new THREE.WebGLRenderer({alpha:true});
 
 renderer.setSize(window.innerWidth,window.innerHeight);
 
@@ -16,36 +15,65 @@ document
 .getElementById("three-container")
 .appendChild(renderer.domElement);
 
-camera.position.z=50;
+camera.position.z = 60;
 
-const nodes=[];
+const nodeMaterial = new THREE.MeshBasicMaterial({
+color:0x38bdf8
+});
 
-const geometry=new THREE.SphereGeometry(0.4,16,16);
+const nodeGeometry = new THREE.SphereGeometry(0.5,16,16);
 
-const material=new THREE.MeshBasicMaterial({color:0x38bdf8});
+fetch("http://127.0.0.1:8000/neural-network")
 
-for(let i=0;i<80;i++){
+.then(res=>res.json())
 
-const node=new THREE.Mesh(geometry,material);
+.then(data=>{
 
-node.position.x=(Math.random()-0.5)*80;
-node.position.y=(Math.random()-0.5)*80;
-node.position.z=(Math.random()-0.5)*80;
+const nodes = [];
+
+data.nodes.forEach(n=>{
+
+const node = new THREE.Mesh(nodeGeometry,nodeMaterial);
+
+node.position.set(n.x,n.y,n.z);
 
 scene.add(node);
 
 nodes.push(node);
 
-}
+});
+
+
+data.edges.forEach(edge=>{
+
+const source = nodes[edge.source];
+const target = nodes[edge.target];
+
+const points = [];
+
+points.push(source.position);
+points.push(target.position);
+
+const geometry =
+new THREE.BufferGeometry().setFromPoints(points);
+
+const material =
+new THREE.LineBasicMaterial({color:0x38bdf8});
+
+const line = new THREE.Line(geometry,material);
+
+scene.add(line);
+
+});
+
+});
+
 
 function animate(){
 
 requestAnimationFrame(animate);
 
-nodes.forEach(node=>{
-node.rotation.x+=0.01;
-node.rotation.y+=0.01;
-});
+scene.rotation.y += 0.002;
 
 renderer.render(scene,camera);
 
